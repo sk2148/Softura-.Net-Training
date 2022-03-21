3bfb1dc57483fff5a5d7f499444e33d8e06e7074
Santhosh kumar s <88973904+sk2148@users.noreply.github.com>
2022-03-21 22:17:47 +0530

    Update day11task2.cs

--- day11task2.cs
+++ day11task2.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,40 +6,38 @@ using System.Threading.Tasks;
 using System.IO;
 namespace ConsoleApp1
 {
+    public class Emp
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Gender { get; set; }
+        public int Age { get; set; }
+    }
     class day11task2
     {
         public static void Main()
         {
-            {
-                FileStream fs = new FileStream
-                        ("C:\\softura\\training\\Employee.txt",
-                        FileMode.OpenOrCreate, FileAccess.ReadWrite);
-                BinaryWriter bw = new BinaryWriter(fs);
-                BinaryWriter bw1 = new BinaryWriter(fs);
-                BinaryWriter bw2 = new BinaryWriter(fs);
-                BinaryWriter bw3 = new BinaryWriter(fs);
-                bw.Write("7894");
-                bw1.Write("SANTHOSH");
-                bw2.Write("21");
-                bw2.Write("Male");
-                bw.Flush();
-                fs.Close();
-                FileStream fs1 = new FileStream(@"C:\softura\training\Employee.txt", FileMode.Open, FileAccess.ReadWrite);
-                BinaryReader br = new BinaryReader(fs1);
-                BinaryReader br1 = new BinaryReader(fs1);
-                BinaryReader br2 = new BinaryReader(fs1);
-                BinaryReader br3 = new BinaryReader(fs1);
-                string str = br.ReadString();
-                string str1 = br1.ReadString();
-                string str2 = br2.ReadString();
-                string str3 = br3.ReadString();
-                Console.WriteLine(str);
-                Console.WriteLine(str1);
-                Console.WriteLine(str2);
-                Console.WriteLine(str3);
-                fs1.Close();
-                Console.Read();
-            }
+
+            FileStream fs = new FileStream("C:\\softura\\training\\Emp.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter bw = new StreamWriter(fs);
+            Emp obj = new Emp();
+            obj.Id =75456;
+            obj.Name = "Dheepak";
+            obj.Gender = "Male";
+            obj.Age = 21;
+            bw.WriteLine(obj.Id);
+            bw.WriteLine(obj.Name);
+            bw.WriteLine(obj.Gender);
+            bw.WriteLine(obj.Age);
+            bw.Flush();
+            fs.Close();
+            FileStream fs1 = new FileStream("C:\\softura\\training\\Emp.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader sr = new StreamReader(fs1);
+            String str = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(str);
+            Console.Read();
 
         }
 
