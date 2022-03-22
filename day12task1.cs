5d4da015b14ce5bb1a59f6a2be0f5bbb8843c9f0
Santhosh kumar s <88973904+sk2148@users.noreply.github.com>
2022-03-22 16:12:17 +0530

    Add files via upload

---
+++ day12task1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+
+{
+    class day12task1
+    {
+        public static void Main()
+        {
+
+            FileStream fs = new FileStream("Restaurant.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            List<string> des = new List<String>();
+            des.Add("Smoke Hub Desserts");
+            des.Add("Coconut Truffle");
+            des.Add("Rasamalai");
+            des.Add("Caramel Custard");
+            des.Add("Bread Halwa");
+            des.Add("Lichi Mousse");
+            foreach (string d in des)
+            {
+                sw.Write("\n" + d);
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
+            FileStream fs1 = new FileStream("Restaurant.txt", FileMode.Open, FileAccess.Read);
+            StreamReader sr = new StreamReader(fs1);
+            String s = sr.ReadToEnd();
+            sr.Close();
+            fs1.Close();
+            Console.WriteLine(s);
+            FileInfo fi = new FileInfo("Restaurant.txt");
+            Console.WriteLine(fi.Length);
+            Console.WriteLine(fi.CreationTime);
+            Console.Read();
+        }
+    }
+}
---
+++ day12task2.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp2
+{
+    [Serializable]
+    class Employee
+    {
+        public string Id = "XYZ89";
+        public String EmpName = "Santhosh Kumar";
+        public string EmpGender = "Male";
+        public int EmpAge = 22;
+    }
+    class Emp
+    {
+        public void SeriToFile()
+        {
+            Employee obj = new Employee();
+            FileStream fs = new FileStream("EmpDetails.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            BinaryFormatter f = new BinaryFormatter();
+            f.Serialize(fs, obj);
+            fs.Close();
+        }
+        public void DeserialData()
+        {
+            FileStream fs = new FileStream("EmpDetails.txt", FileMode.Open, FileAccess.Read);
+            BinaryFormatter f = new BinaryFormatter();
+            Employee ed = (Employee)f.Deserialize(fs);
+            Console.WriteLine(ed.Id);
+            Console.WriteLine(ed.EmpName);
+            Console.WriteLine(ed.EmpGender);
+            Console.WriteLine(ed.EmpAge);
+        }
+
+        public static void Main()
+        {
+            Emp eobj = new Emp();
+            eobj.SeriToFile();
+            eobj.DeserialData();
+            Console.Read();
+        }
+
+    }
+}
