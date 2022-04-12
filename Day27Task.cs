d23daeaef3af6aee5bd5c1d46bb9860de15fda62
Santhosh kumar s <88973904+sk2148@users.noreply.github.com>
2022-04-12 20:39:16 +0530

    Update Day27Task.cs

--- Day27Task.cs
+++ Day27Task.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Data.SqlClient;
@@ -9,40 +9,39 @@ namespace Dataa
     class Enrol_tbl
     {
         SqlConnection con = new SqlConnection("data source=DESKTOP-VUVG2NF;database=Trail;user id=sa;password=P@ssw0rd");
-        public void Enroll_Insert()
+        public void Enroll_Insert(int id,string fname,string lname,char gender,string email,string phone)
         {
-            SqlConnection con = new SqlConnection("data source=DESKTOP-VUVG2NF;database=Trail;user id=sa;password=P@ssw0rd");
-            string sqlqry = "insert tbl_enrollment values('Shanmuga','Sanjai','M','sj7489','9988455667')";
+            string sqlqry = "insert tbl_enroll values("+id+",'"+fname+"','"+lname+"','"+gender+"','"+email+"','"+phone+"')";
             SqlCommand cmd = new SqlCommand(sqlqry, con);
             con.Open();
             cmd.ExecuteNonQuery();
             con.Close();
             Console.WriteLine("Row Inserted");
         }
-        public void Enroll_Update()
+        public void Enroll_Update(int id,string lname)
         {
-            SqlConnection con = new SqlConnection("data source=DESKTOP-VUVG2NF;database=Trail;user id=sa;password=P@ssw0rd");
-            string sqlqry = "update tbl_enrollment set StuFname='Sanjai',StuLname='J'where StudentID=2002";
+            string sqlqry = "update tbl_enroll set Lname='"+lname+"' where Id="+id+"";
             SqlCommand cmd = new SqlCommand(sqlqry, con);
             con.Open();
             cmd.ExecuteNonQuery();
             con.Close();
             Console.WriteLine("Row Updated");
         }
-        public void Enroll_Delete()
+        public void Enroll_Delete(int id)
         {
             SqlConnection con = new SqlConnection("data source=DESKTOP-VUVG2NF;database=Trail;user id=sa;password=P@ssw0rd");
-            string sqlqry = "delete tbl_enrollment where StudentID=2002";
+            string sqlqry = "delete tbl_enroll where Id="+id+"";
             SqlCommand cmd = new SqlCommand(sqlqry, con);
             con.Open();
             cmd.ExecuteNonQuery();
             con.Close();
             Console.WriteLine("Row Deleted");
         }
-        public void InsSp(string fname,string lname,char gender,string email,string phone)
+        public void InsSp(int Id,string fname,string lname,char gender,string email,string phone)
         {
             SqlCommand cmd = new SqlCommand("sp_InsEnroll", con);
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
             cmd.Parameters.Add("@fname", SqlDbType.VarChar).Value = fname;
             cmd.Parameters.Add("@lname", SqlDbType.VarChar).Value = lname;
             cmd.Parameters.Add("@gender", SqlDbType.Char).Value = gender;
@@ -53,13 +52,15 @@ namespace Dataa
             con.Close();
             Console.WriteLine("Row Inserted using Stored Procedure");
         }
-        public void UpdSp(int Id,string fname,string lname)
+        public void UpdSp(int Id,string lname)
         {
             SqlCommand cmd = new SqlCommand("sp_UpdEnroll", con);
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
-            cmd.Parameters.Add("@fname", SqlDbType.VarChar).Value = fname;
             cmd.Parameters.Add("@lname", SqlDbType.VarChar).Value = lname;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
             Console.WriteLine("Row Updated Using Stored Procedure");
         }
         public void DelSp(int Id)
@@ -67,29 +68,53 @@ namespace Dataa
             SqlCommand cmd = new SqlCommand("sp_DelEnroll", con);
             cmd.CommandType = System.Data.CommandType.StoredProcedure;
             cmd.Parameters.Add("@Id", SqlDbType.Int).Value = Id;
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
             Console.WriteLine("Row Deleted Using Stored Procedure");
         }
         public static void Main()
         {
             Enrol_tbl obj = new Enrol_tbl();
-            Console.WriteLine("Enter the ID:");
-            int Id = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the First Name:");
-            string fname = Console.ReadLine();
-            Console.WriteLine("Enter the Last Name:");
-            string lname = Console.ReadLine();
-            Console.WriteLine("Enter the Gender:");
-            char gender = Convert.ToChar(Console.ReadLine());
-            Console.WriteLine("Enter the EmailID:");
-            string email = Console.ReadLine();
-            Console.WriteLine("Enter the Phone Number:");
-            string phone = Console.ReadLine();
-            obj.Enroll_Insert();
-            obj.InsSp(fname, lname, gender, email, phone);
-            obj.Enroll_Update();
-            obj.UpdSp(Id, fname, lname);
-            obj.Enroll_Delete();
-            obj.DelSp(Id);
+            Console.WriteLine("Enter the Function To be Performed:" + "\n" +"1.Insert"+"\n"+"2.Update"+"\n"+"3.Delete");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice == 1)
+            {
+                Console.WriteLine("Enter the ID:");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the First Name:");
+                string fname = Console.ReadLine();
+                Console.WriteLine("Enter the Last Name:");
+                string lname = Console.ReadLine();
+                Console.WriteLine("Enter the Gender:");
+                char gender = Convert.ToChar(Console.ReadLine());
+                Console.WriteLine("Enter the EmailID:");
+                string email = Console.ReadLine();
+                Console.WriteLine("Enter the Phone Number:");
+                string phone = Console.ReadLine();
+                obj.Enroll_Insert(id, fname,lname,gender,email,phone);
+                //obj.InsSp(id, fname, lname, gender, email, phone);
+            }
+            else
+                if (choice == 2)
+            {
+                Console.WriteLine("Enter the Id :");
+                int id = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Last Name To be Changed:");
+                string lname = Console.ReadLine();
+                //obj.Enroll_Update(id, lname);
+                obj.UpdSp(id, lname);
+            }
+            else
+                if (choice == 3)
+            {
+                Console.WriteLine("Enter The Id To be Deleted:");
+                int Id = Convert.ToInt32(Console.ReadLine());
+                //obj.Enroll_Delete(Id);
+                obj.DelSp(Id);
+            }
+            else
+                Console.WriteLine("Invalid Operation");
         }
     }
 }
