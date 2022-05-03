30cedfecdec68fb66aa5ac92dd8d060f52fa60dd
Santhosh kumar s <88973904+sk2148@users.noreply.github.com>
2022-05-03 21:59:18 +0530

    Add files via upload

---
+++ 3rdMayTask/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using AdoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using AdoMVC.DAL;
+
+namespace AdoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            CustomerDAL custDAL = new CustomerDAL();
+            List<Customer> CustomerList = new List<Customer>();
+            CustomerList = custDAL.GetAllCustomers();
+            return View(CustomerList);
+        }
+        public IActionResult Create()
+        {
+            CustomerDAL custDAL = new CustomerDAL();
+            List<Customer> CustomerList = new List<Customer>();
+            CustomerList = custDAL.NewCustomer("Sekar","sv67@gmail.com","7855469954");
+            return Content("Saved");
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 3rdMayTask/DAL/CustomerDAL.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using AdoMVC.Models;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace AdoMVC.DAL
+{
+    public class CustomerDAL
+    {
+        public string cnn = "";
+        public CustomerDAL()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath
+                (Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json").Build();
+            cnn = builder.GetSection("ConnectionStrings:Conn").Value;
+        }
+        public List<Customer> GetAllCustomers()
+        {
+            List<Customer> listCustomer = new List<Customer>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("GetAllCustomers", con))
+                {
+                    if (con.State == System.Data.ConnectionState.Closed)
+                        con.Open();
+                    IDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        listCustomer.Add(new Customer()
+                        {
+                            CustomerID = int.Parse(reader["CustomerID"].ToString()),
+                            CustomerName = reader["CustomerName"].ToString(),
+                            EmailID = reader["EmailID"].ToString(),
+                            MobileNo = reader["MobileNo"].ToString()
+                        }); ;
+                    }
+                }
+            }
+            return listCustomer;
+        }
+        public List<Customer> NewCustomer(string cus, string em, string mb)
+        {
+            List<Customer> listCustomer = new List<Customer>();
+            using (SqlConnection con = new SqlConnection(cnn))
+            {
+                using (SqlCommand cmd = new SqlCommand("NewCustomer", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@cname",SqlDbType.VarChar).Value=cus;
+                    cmd.Parameters.Add("@email", SqlDbType.VarChar).Value = em;
+                    cmd.Parameters.Add("@mobno", SqlDbType.VarChar).Value = mb;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            return listCustomer;
+        }
+    }
+}
