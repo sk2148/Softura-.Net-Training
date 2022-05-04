bc103a971423f03b8637922f9eab1f6e3d69cedc
Santhosh kumar s <88973904+sk2148@users.noreply.github.com>
2022-05-04 16:31:00 +0530

    Add files via upload

---
+++ 4thMay Task/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdoMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
